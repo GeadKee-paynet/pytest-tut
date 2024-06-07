0d1c89e7dbd1c6b318e4fc191a0c5754c17a9d36
Gan Gead Kee <geadkee.gan@paynet.my>
2024-06-07 16:54:18 +0800

    done chap3, equivalence, but my code doesn't work like how the tutor showed. :(

--- test/test_math.py
+++ test/test_math.py
@@ -1,5 +1,19 @@
+import pytest
+
+
 def testOnePlusOne():
     assert 1 + 1 == 2
-    
+
+def testOnePlusTwo():
+    a = 1
+    b = 2
+    c = 3
+    assert a + b == c
+
+def divideByZero():
+    with pytest.raises(ZeroDivisionError) as e:
+        num = 1 / 0
+
+    assert 'division by zero' in str(e.value) 
 
 
