cb3572438a6987ae99bd5232a435216b1c20b156
Gan Gead Kee <geadkee.gan@paynet.my>
2024-06-10 11:48:44 +0800

    done chap 4

--- test/test_math.py
+++ test/test_math.py
@@ -10,10 +10,14 @@ def testOnePlusTwo():
     c = 3
     assert a + b == c
 
-def divideByZero():
-    with pytest.raises(ZeroDivisionError) as e:
-        num = 1 / 0
+def testDivideByZero():
+  with pytest.raises(ZeroDivisionError) as e:
+    num = 1 / 0
 
-    assert 'division by zero' in str(e.value) 
+  assert "division by zero" in str(e.value) 
 
-
+# def test_divide_by_zero():
+#   with pytest.raises(ZeroDivisionError) as e:
+#     num = 1 / 0
+  
+#   assert 'division by zero' in str(e.value)
---
+++ test/test_math2.py
@@ -0,0 +1,18 @@
+import pytest
+
+## pytest.mark,parametrize
+
+
+products = [
+  (2, 3, 5),            # postive integers
+  (1, 99, 99),          # identity
+  (0, 99, 0),           # zero
+  (3, -4, -12),         # positive by negative
+  (-5, -5, 25),         # negative by negative
+  (2.5, 6.7, 16.75)     # floats
+]
+
+@pytest.mark.parametrize("a, b, product", products)
+def test_multiplication(a, b, product):
+    assert a * b == product
+
